2066084f238dc7f6723713a14e5d1dbf14330971
agustiyan <agustiyaninfo@gmail.com>
2022-02-10 17:49:06 +0700

    fix show users data

--- lib/screen/third_screen.dart
+++ lib/screen/third_screen.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:km_test/model/user_model.dart';
+import 'package:km_test/services/api_service.dart';
 
-class ThirdScreen extends StatelessWidget {
+class ThirdScreen extends StatefulWidget {
   const ThirdScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final List<String> entries = <String>['A', 'B', 'C'];
+  State<ThirdScreen> createState() => _ThirdScreenState();
+}
 
+class _ThirdScreenState extends State<ThirdScreen> {
+  List<User> _users = <User>[];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    populateUsers();
+  }
+
+  void populateUsers() async {
+    final users = await fetchUsers();
+    setState(() {
+      _users = users;
+      isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -20,60 +42,69 @@ class ThirdScreen extends StatelessWidget {
           color: Colors.black,
         ),
       ),
-      body: ListView.builder(
-        padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-        itemCount: entries.length,
-        itemBuilder: (context, index) {
-          return Container(
-            height: 80,
-            decoration: const BoxDecoration(
-              border: Border(
-                bottom: BorderSide(width: 0.5, color: Color(0xFFE2E3E4)),
-              ),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                const SizedBox(width: 18),
-                Container(
-                  width: 49,
-                  height: 49,
+      body: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+              itemCount: _users.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  height: 80,
                   decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage(
-                        'assets/background.png',
-                      ),
+                    border: Border(
+                      bottom: BorderSide(width: 0.5, color: Color(0xFFE2E3E4)),
                     ),
                   ),
-                ),
-                const SizedBox(width: 20),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      entries[index],
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      const SizedBox(width: 18),
+                      Container(
+                        width: 49,
+                        height: 49,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                              _users[index].avatar,
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
-                    Text(
-                      entries[index],
-                      style: TextStyle(
-                        fontSize: 10,
-                        fontWeight: FontWeight.w500,
+                      const SizedBox(width: 20),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            _users[index].firstName +
+                                ' ' +
+                                _users[index].lastName,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Text(
+                            _users[index].email,
+                            textAlign: TextAlign.left,
+                            style: const TextStyle(
+                              fontSize: 10,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
     );
   }
 }
